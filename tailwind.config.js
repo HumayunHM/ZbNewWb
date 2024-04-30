cc6560a7dd0564b4cf8bffdfb6590279939f6790
humayun <humayunharoon6@gmail.com>
2024-05-01 03:01:54 +0500

    before changing

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+            'maintext' : '#001042',
+            'lightblue' : '#7ED6DF',
+            'darkerblue' : '#22A6B3',
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
