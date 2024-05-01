39e82c642e0a13d9eb1c2908438564551fc5cb15
humayun <humayunharoon6@gmail.com>
2024-05-01 21:10:06 +0500

    fix bottom button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
   document.getElementById("google-forms-button").addEventListener("click", function() {
     window.open("https://forms.gle/5xQDtTm9uGzsLvsz7", "_blank");
 });
+
+document.getElementById("google-forms-button2").addEventListener("click", function() {
+  window.open("https://forms.gle/5xQDtTm9uGzsLvsz7", "_blank");
+});
